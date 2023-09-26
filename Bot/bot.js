318cb4fd415ad54a1d530ae8678f25c37fe98d8e
dev-aly3n <dev.aly3n@gmail.com>
2023-09-26 14:48:18 +0330

    first commit

---
+++ Bot/bot.js
@@ -0,0 +1,6 @@
+const { Telegraf } = require('telegraf')
+const { message } = require('telegraf/filters')
+
+const bot = new Telegraf("6519061219:AAFv52vIofzoApNzT2gFfID2x-I8zAcy6UQ")
+bot.start((ctx) => ctx.reply('Welcome mf',{reply_markup:{keyboard:[[{text:"web app",web_app:""}]]}}))
+bot.launch()
