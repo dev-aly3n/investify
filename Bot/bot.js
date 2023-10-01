64af4d84ad48798a29f90e42ddd45035807378e0
dev-aly3n <dev.aly3n@gmail.com>
2023-10-01 10:23:53 +0330

    add prebuild setup

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,6 +1,6 @@
 const { Telegraf } = require('telegraf')
 const { message } = require('telegraf/filters')
 
-const bot = new Telegraf("6519061219:AAFv52vIofzoApNzT2gFfID2x-I8zAcy6UQ")
+const bot = new Telegraf("6519061219:AAFuQ-I96SAAx5ySErpN7OL0pRZ1TY4yH9o")
 bot.start((ctx) => ctx.reply('Welcome',{reply_markup:{keyboard:[[{text:"web app",web_app:{url:"https://investiiify.vercel.app/"}}]]}}))
 bot.launch()
